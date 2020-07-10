18bf0902db9a07272357a88e408fa8dcb979de7d
tbremm <timothy.bremm@gmail.com>
2020-07-10 10:28:04 -0700

    Completed string intro

---
+++ string-intro.cpp
@@ -0,0 +1,21 @@
+// https://www.hackerrank.com/challenges/c-tutorial-strings/problem
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b;
+    char temp;
+    cin >> a;
+    cin >> b;
+
+    cout << a.length() << ' ' << b.length() << endl;;
+    cout << a + b << endl;
+    temp = a[0];
+    a[0] = b[0];
+    b[0] = temp;
+    cout << a << ' ' << b;
+  
+    return 0;
+}
